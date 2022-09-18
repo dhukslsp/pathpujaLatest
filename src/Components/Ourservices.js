fafcf2870d7ba02c63a3a152ff3d0111ac5e099c
Dhruv SIngh <dhruv1647>
2022-09-18 20:01:49 +0530

    Services  page

---
+++ src/Components/Ourservices.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react'
+import Navbar from './Navbar'
+export default class Ourservices extends Component {
+    render() {
+        return (
+            <div className='bg-white'>
+                <Navbar />
+                <p className='text-2xl text-black font-bold text-center pt-1'>Choose Your Pooja From here</p>
+                <div className='flex flex-wrap sm:flex-col lg:flex-row justify-center mt-8'>
+                    <ul>
+                        <li className="dropdown lg:inline sm:block px-4 text-black hover:text-purple-700 cursor-pointer font-bold text-base uppercase tracking-wide">
+                            <a>Puja</a>
+                            <div className="dropdown-menu  hidden h-auto flex pt-4">
+                                <ul className="block w-full bg-white shadow px-12 py-8">
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 2</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 3</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 4</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 5</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li className="dropdown2 lg:inline sm:block px-4 text-black hover:text-purple-700 cursor-pointer font-bold text-base uppercase tracking-wide">
+                            <a>PATH/HAWAN</a>
+                            <div className="dropdown-menu2  hidden h-auto flex pt-4">
+                                <ul className="block w-full bg-white shadow px-12 py-8">
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 2</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 3</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 4</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 5</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li className="dropdown2 lg:inline sm:block px-4 text-black hover:text-purple-700 cursor-pointer font-bold text-base uppercase tracking-wide">
+                            <a>JAAP</a>
+                            <div className="dropdown-menu2  hidden h-auto flex pt-4">
+                                <ul className="block w-full bg-white shadow px-12 py-8">
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 2</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 3</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 4</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 5</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li className="dropdown2 lg:inline sm:block px-4 text-black hover:text-purple-700 cursor-pointer font-bold text-base uppercase tracking-wide">
+                            <a>Festival Puja</a>
+                            <div className="dropdown-menu2  hidden h-auto flex pt-4">
+                                <ul className="block w-full bg-white shadow px-12 py-8">
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 2</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 3</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 4</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 5</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li className="dropdown2 lg:inline sm:block px-4 text-black hover:text-purple-700 cursor-pointer font-bold text-base uppercase tracking-wide">
+                            <a>Shanti Puja</a>
+                            <div className="dropdown-menu2  hidden h-auto flex pt-4">
+                                <ul className="block w-full bg-white shadow px-12 py-8">
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 2</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 3</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 4</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 5</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                    <ul>
+                       <li className="dropdown2 lg:inline sm:block px-4 text-black hover:text-purple-700 cursor-pointer font-bold text-base uppercase tracking-wide">
+                            <a>PITRA Puja</a>
+                            <div className="dropdown-menu2  hidden h-auto flex pt-4">
+                                <ul className="block w-full bg-white shadow px-12 py-8">
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 2</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 3</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 4</a></li>
+                                    <li className="py-1"><a className="block text-black font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 5</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
