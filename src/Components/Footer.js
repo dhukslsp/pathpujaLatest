171dc1851a09d3c34e6202f5dc6ffc195eac5781
Dhruv SIngh <dhruv1647>
2022-10-02 01:36:48 +0530

    Latest

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -24,7 +24,7 @@ function Footer() {
                                 d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z">
                             </path>
                         </svg>
-                    </a>                    
+                    </a>
                     <a href="https://www.instagram.com/invites/contact/?i=1f1twpnounouw&utm_content=os6tp7l" target="_blank" className="mr-6 text-gray-600">
                         <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="instagram"
                             className="w-3.5" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
@@ -57,8 +57,7 @@ function Footer() {
                             Tailwind ELEMENTS
                         </h6>
                         <p>
-                            Here you can use rows and columns to organize your footer content. Lorem ipsum dolor
-                            sit amet, consectetur adipisicing elit.
+                            Pathpuja is a priest service providing company, crafted with the intention of connecting individuals to the  divine soul or inner self. To help customers celebrate their auspicous and special ocassion through pujas and havan. Providing quality services of highly qualified pandits and smooth puja process at affordable prices. The booking process is  easy and less time. The ultimate intention is to create a memorable puja experience for you and your loved ones
                         </p>
                     </div>
                     <div className="">
@@ -84,7 +83,7 @@ function Footer() {
                             <a href="#!" className="text-gray-600">Pitru-Paksha Puja</a>
                         </p>
                     </div>
-                    
+
                     <div className="">
                         <h6 className="uppercase font-semibold mb-4 flex justify-center md:justify-start">
                             Contact
@@ -110,7 +109,7 @@ function Footer() {
                             +91 9509238340
                         </p>
                         <p className="flex items-center justify-center md:justify-start">
-                            <img src="/images/whatsapp-svgrepo-com.svg" alt="" srcset="" style={{"height":"18px","margin-right": "0.9pc"}}/>
+                            <img src="/images/whatsapp-svgrepo-com.svg" alt="" srcset="" style={{ "height": "18px", "margin-right": "0.9pc" }} />
                             +91 9509238340
                         </p>
                     </div>
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,36 +1,56 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Footer from './Footer'
 function HomePage() {
     return (
-        <div className='homequery h-full flex flex-row'>
-            <div style={{ "backgroundColor": "#fcf5eb", "height": "50pc" }} className="h-full" >
-                <img src="/images/A2.png" alt="ganesh Ji Image" className="p-0"/>
-                <div className='p-10'>
-                    <img src="/images/A3.png" alt="there is always room for pathpuja" className="p-0 ml-auto mr-auto mb-10" style = {{"height":"12pc","marginTop":"16px"}}/>
-                    <img src="/images/A4.png" alt="Sub Nav" className="p-0" />
-                </div>
-                <div className='w-full flex justify-center'>
-                    <input type="search" className='ml-auto mr-2 p-4' style = {{"border":"3px solid #ba4b2f","color":"#ba4b2f","backgroundColor":"rgb(252 245 235)"}} placeholder="Enter Puja Hawan Etc"/>
-                    <button className='p-4 ml-10 mr-auto' style = {{"border":"3px solid #ba4b2f","color":"#ba4b2f","backgroundColor":"rgb(252 245 235)","fontSize":"21px"}}>View Services</button>
-                </div>
-            </div>
-            <div style={{ "backgroundColor": "#efa593" }} >
-                <div>
-                    <div className="navbar bg-base-100 font-extrabold mt-5" style={{ "backgroundColor": "#efa593" }}>
-                        <div className="flex-1">
+        <div>
+            <div className='bg-white text-black'>
+                <div className='homequery h-full flex flex-row'>
+                    <div style={{ "backgroundColor": "#fcf5eb", "height": "50pc" }} >
+                        <img src="/images/A2.png" alt="ganesh Ji Image" className="p-0" />
+                        <div className='p-10'>
+                            <img src="/images/A3.png" alt="there is always room for pathpuja" className="p-0 ml-auto mr-auto mb-10" style={{ "height": "12pc", "marginTop": "16px" }} />
+                            <img src="/images/A4.png" alt="Sub Nav" className="p-0" />
                         </div>
-                        <div className="flex-none">
-                            <ul className="menu menu-horizontal p-0 font-extrabold text-xl text-black">
-                                <li><Link to = "/">Home</Link></li>
-                                <li><Link to = "/Services">Services</Link></li>
-                                <li><Link to = "/About">About Us</Link></li>
-                                <li><img className='p-2' src="/images/WhatsApp.png" alt="" style={{ "height": "3pc", "paddingTop": "11px" }} /></li>
-                            </ul>
+                        <div className='w-full flex justify-center flex-wrap divElements'>
+                            <input type="search" className='ml-auto lg:mr-2 p-4 mt-8 sm:mr-auto ' style={{ "border": "3px solid #ba4b2f", "color": "#ba4b2f", "backgroundColor": "rgb(252 245 235)","width":"22pc" }} placeholder="Enter Puja Hawan Etc" />
+                            <button className='p-4 lg:ml-10 mr-auto mt-8' id = "homeBut" style={{ "border": "3px solid #ba4b2f", "color": "#ba4b2f", "backgroundColor": "rgb(252 245 235)", "fontSize": "21px" }}><Link to = "/Services">View Services</Link></button>
                         </div>
                     </div>
+                    <div style={{ "backgroundColor": "#efa593" }} >
+                        <div>
+                            <div className="navbar bg-base-100 font-extrabold mt-5" style={{ "backgroundColor": "#efa593" }}>
+                                <div className="flex-1">
+                                </div>
+                                <div className="flex-none">
+                                    <ul className="menu menu-horizontal p-0 font-extrabold text-xl text-black">
+                                        <li><Link to="/">Home</Link></li>
+                                        <li><Link to="/Services">Services</Link></li>
+                                        <li><Link to="/About">About Us</Link></li>
+                                        <li><img className='p-2' src="/images/WhatsApp.png" alt="" style={{ "height": "3pc", "paddingTop": "11px" }} /></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <img src="/images/A1.png" alt="ganesh Ji Image" className="p-20 pt-5" />
+                    </div>
                 </div>
-                <img src="/images/A1.png" alt="ganesh Ji Image" className="p-20 pt-5" />
+                <div style = {{"background":"#efa593","border-top": "6px solid black;","width":"full"}}>
+                    <p className='text-center pt-7 font-extrabold' style={{ "color": "#ba4b2f", "fontSize": "48px", }}>Services</p>
+                    <div className='flex flex-row justify-center flex-wrap'>
+                    <div className='h-full p-10'>
+                        <img src="/images/S.png" alt="" style={{"height":"30pc"}}/>
+                    </div>
+                    <div className='h-full p-10'>
+                        <img src="/images/Sa.png" alt="" style={{"height":"30pc"}}/>
+                    </div>
+                    <div className='h-full p-10'>
+                        <img src="/images/sb.png" alt="" style={{"height":"30pc"}}/>
+                    </div>
+                    </div>
+                </div>
             </div>
+            <Footer />
         </div>
     )
 }
