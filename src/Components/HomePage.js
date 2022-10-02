c8e5606621ed5be45f94c2f1fa2d7f5c70842c35
Dhruv SIngh <dhruv1647>
2022-10-02 12:08:47 +0530

    Made new status

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -13,8 +13,8 @@ function HomePage() {
                             <img src="/images/A4.png" alt="Sub Nav" className="p-0" />
                         </div>
                         <div className='w-full flex justify-center flex-wrap divElements'>
-                            <input type="search" className='ml-auto lg:mr-2 p-4 mt-8 sm:mr-auto ' style={{ "border": "3px solid #ba4b2f", "color": "#ba4b2f", "backgroundColor": "rgb(252 245 235)","width":"22pc" }} placeholder="Enter Puja Hawan Etc" />
-                            <button className='p-4 lg:ml-10 mr-auto mt-8' id = "homeBut" style={{ "border": "3px solid #ba4b2f", "color": "#ba4b2f", "backgroundColor": "rgb(252 245 235)", "fontSize": "21px" }}><Link to = "/Services">View Services</Link></button>
+                            <input type="search" className='ml-auto lg:mr-2 p-4 mt-8 sm:mr-auto ' style={{ "border": "3px solid #ba4b2f", "color": "#ba4b2f", "backgroundColor": "rgb(252 245 235)", "width": "22pc" }} placeholder="Enter Puja Hawan Etc" />
+                            <button className='p-4 lg:ml-10 mr-auto mt-8' id="homeBut" style={{ "border": "3px solid #ba4b2f", "color": "#ba4b2f", "backgroundColor": "rgb(252 245 235)", "fontSize": "21px" }}><Link to="/Services">View Services</Link></button>
                         </div>
                     </div>
                     <div style={{ "backgroundColor": "#efa593" }} >
@@ -35,18 +35,49 @@ function HomePage() {
                         <img src="/images/A1.png" alt="ganesh Ji Image" className="p-20 pt-5" />
                     </div>
                 </div>
-                <div style = {{"background":"#efa593","border-top": "6px solid black;","width":"full"}}>
+                <div style={{ "background": "#efa593", "border-top": "6px solid black;", "width": "full" }}>
                     <p className='text-center pt-7 font-extrabold' style={{ "color": "#ba4b2f", "fontSize": "48px", }}>Services</p>
                     <div className='flex flex-row justify-center flex-wrap'>
-                    <div className='h-full p-10'>
-                        <img src="/images/S.png" alt="" style={{"height":"30pc"}}/>
+                        <div className='h-full p-10'>
+                            <img src="/images/S.png" alt="" style={{ "height": "30pc" }} />
+                        </div>
+                        <div className='h-full p-10'>
+                            <img src="/images/Sa.png" alt="" style={{ "height": "30pc" }} />
+                        </div>
+                        <div className='h-full p-10'>
+                            <img src="/images/sb.png" alt="" style={{ "height": "30pc" }} />
+                        </div>
                     </div>
-                    <div className='h-full p-10'>
-                        <img src="/images/Sa.png" alt="" style={{"height":"30pc"}}/>
+                </div>
+                <div style={{ "background": "#fcf5eb", "border-top": "6px solid black;", "width": "full" }} className="newcolor">
+                    <p className='text-center pt-7 font-extrabold' style={{ "color": "#ba4b2f", "fontSize": "48px", }}>Services</p>
+                    <div className='flex justify-center flex-wrap'>
+                        <div className='h-full p-10 ml-auto mr-auto flex justify-center flex-col'>
+                            <img src="/images/homeing/Sc1.png" alt="" style={{ "height": "7pc" ,"alignSelf":"center"}} />
+                            <p className='mt-7 text-xl font-extrabold'>Easy Booking</p>
+                        </div>
+                        <div className='h-full p-10 ml-auto mr-auto flex justify-center flex-col'>
+                            <img src="/images/homeing/Sc2.png" alt="" style={{ "height": "7pc" ,"alignSelf":"center"}} />
+                            <p className='mt-7 text-xl font-extrabold'>Modest Pricing</p>
+                        </div>
+                        <div className='h-full p-10 ml-auto mr-auto flex justify-center flex-col'>
+                            <img src="/images/homeing/Sc3.png" alt="" style={{ "height": "7pc" ,"alignSelf":"center"}} />
+                            <p className='mt-7 text-xl font-extrabold'>Professional Team</p>
+                        </div>
                     </div>
-                    <div className='h-full p-10'>
-                        <img src="/images/sb.png" alt="" style={{"height":"30pc"}}/>
-                    </div>
+                    <div className='flex justify-center flex-wrap'>
+                        <div className='h-full p-10 ml-auto mr-auto flex justify-center flex-col'>
+                            <img src="/images/homeing/Sc4.png" alt="" style={{ "height": "7pc" ,"alignSelf":"center"}} />
+                            <p className='mt-7 text-xl font-extrabold'>Full Guidance <br/> and Support</p>
+                        </div>
+                        <div className='h-full p-10 ml-auto mr-auto flex justify-center flex-col'>
+                            <img src="/images/homeing/Sc5.png" alt="" style={{ "height": "7pc" ,"alignSelf":"center"}} />
+                            <p className='mt-7 text-xl font-extrabold'>Experienced priest</p>
+                        </div>
+                        <div className='h-full p-10 ml-auto mr-auto flex justify-center flex-col'>
+                            <img src="/images/homeing/Sc6.png" alt="" style={{ "height": "7pc" ,"alignSelf":"center"}} />
+                            <p className='mt-7 text-xl font-extrabold width-auto'>Puntuality Promse</p>
+                        </div>
                     </div>
                 </div>
             </div>
