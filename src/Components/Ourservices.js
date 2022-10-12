6dda55215d3aae18a9cf308269062969588d8d20
Dhruv SIngh <dhruv1647>
2022-10-12 11:48:31 +0530

    Further optimisations 1

--- src/Components/Ourservices.js
+++ src/Components/Ourservices.js
@@ -15,7 +15,7 @@ function Ourservices() {
                     <li id="allbtn" className='pl-0'><a onClick={()=>{chpuja(0)}}>PUJA</a></li>
                     <li id="dubbtn" className='pl-0'><a onClick={()=>{chpuja(1)}}>JAAP</a></li>
                     <li id="recentbtn" className='pl-0'><a onClick={()=>{chpuja(2)}}>PATH/HAVAN</a></li>
-                    <li id="followedbtn" className='pl-0'><a onClick={()=>{chpuja(3)}}>FESTIVL PUJA</a></li>
+                    <li id="followedbtn" className='pl-0'><a onClick={()=>{chpuja(3)}}>FESTIVAL PUJA</a></li>
                     <li id="popularbtn" className='pl-0'><a onClick={()=>{chpuja(4)}}>SHANTI PUJA</a></li>
                     <li id="moviebtn" className='pl-0'><a onClick={()=>{chpuja(5)}}>PITRA PUJA</a></li>
                 </ul>
